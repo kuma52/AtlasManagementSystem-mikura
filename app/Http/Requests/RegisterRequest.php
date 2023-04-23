ff9640f835ada00102e2b9e8ed4c7ea0c1480f63
kuma52 <i.am.kuma52@gmail.com>
2023-04-23 13:52:21 +0900

    軽微な修正

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -23,8 +23,6 @@ class RegisterRequest extends FormRequest
      */
     public function getValidatorInstance(){
         //プルダウンで選択された値を取得('フォームで送信されたkey','value')
-        // $datetime = $this->input(['old_year', 'old_month', 'old_day'], array());//valueのデフォは空の配列
-        //$datetime = ['old_year', 'old_month', 'old_day'];
         $datetime = [
             $this-> input('old_year'),
             $this-> input('old_month'),
@@ -58,7 +56,7 @@ class RegisterRequest extends FormRequest
             // 'old_year' => 'required|after:1999',
             // 'old_month' => 'required|present',
             // 'old_day' => 'required|before:today',
-            'datetime_validation' => 'required|date|after:1999-12-31|before:today',
+            'datetime_validation' => 'required|date|after:1999-12-31|before:today',//インスタンスで作ったやつ
             'role' => 'required|in:1,2,3,4',//1,2,3以外の値は認めないよの記述
             'password' => 'required|between:8,30|confirmed',
         ];
@@ -79,27 +77,33 @@ class RegisterRequest extends FormRequest
             'over_name_kana.string' => '無効な入力です',
             'over_name_kana.max' => 'セイは30文字以内で入力してください',
             'over_name_kana.regex' => 'カタカナで入力ください',
+
             //ナマエ
             'under_name_kana.required' => '入力は必須です',
             'under_name_kana.string' => '無効な入力です',
             'under_name_kana.max' => 'メイは30文字以内で入力してください',
             'under_name_kana.regex' => 'カタカナで入力ください',
+
             //address
             'mail_address.required' => '入力は必須です',
             'mail_address.max' => '100文字以内で入力してください',
             'mail_address.email' => '形式が無効です',
             'mail_address.unique' => '登録済みのアドレスです',
+
             //性別
             'sex.required' => '入力は必須です',
             'sex.in' => '無効な値です',
+
             //生年月日
             'datetime_validation.required' => '入力は必須です',
             'datetime_validation.after' => '2000年以降の日付にしてください',
             'datetime_validation.before' => '今日までの日付で入力してください',
             'datetime_validation.date' => '無効な日付です',
+
             //役割
             'role.required' => '入力は必須です',
             'role.in' => '無効な値です',
+
             //パスワード
             'password.required' => '入力は必須です',
             'password.between' => '8~30文字にしてください',
