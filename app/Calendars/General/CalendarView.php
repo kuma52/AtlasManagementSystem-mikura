286af68e0fcb6d94cb758cce2281d74a35c8cbeb
kuma52 <i.am.kuma52@gmail.com>
2023-05-06 17:45:02 +0900

    「リモ●部参加」の文言を追加修正

--- app/Calendars/General/CalendarView.php
+++ app/Calendars/General/CalendarView.php
@@ -50,6 +50,7 @@ class CalendarView{
         };
         $html[] = $day->render();
 
+        //予約をしている場合
         if(in_array($day->everyDay(), $day->authReserveDay())){
           $reservePart = $day->authReserveDate($day->everyDay())->first()->setting_part;
           if($reservePart == 1){
@@ -59,15 +60,19 @@ class CalendarView{
           }else if($reservePart == 3){
             $reservePart = "リモ3部";
           }
-          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px"></p>';
+          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){//過去
+            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">'. $reservePart .'参加</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
-          }else{
+          }else{//未来
             $html[] = '<button type="submit" class="modal-open btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" data-bs-target="#cancelModal" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="deleteParts">';
           }
-        }else{
+        }else{//予約していない場合
+          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){//過去
+            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px">受付終了</p>';
+          }else{//未来
           $html[] = $day->selectPart($day->everyDay());
+          }
         }
         $html[] = $day->getDate();
         $html[] = '</td>';
@@ -79,18 +84,18 @@ class CalendarView{
     $html[] = '</div>';
     $html[] = '<form action="/reserve/calendar" method="post" id="reserveParts">'.csrf_field().'</form>';
 
-    // 予約キャンセルのモーダル
-    $html[] = '<div class="modal" id="cancelModal">';
-    $html[] = '<div class="modal__bg"></div>';
-    $html[] = '<div class="modal__content">';
-    $html[] = '<p>ファサードとかでモーダルに引っ張った日時を表示</p>';
-    $html[] = '<p class=".modal-part"></p>';
-    $html[] = '<p>上記の予約をキャンセルしてもよろしいですか？</p>';
-    $html[] = '<input type="hidden" name="id" value="" form="deleteParts">';
-    $html[] = '<form action="/delete/calendar" form="deleteParts" method="post"></form>';
-    $html[] = '<button class="modal-close btn btn-primary p-0 w-75">閉じる</button>';
-    $html[] = '<button class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" form="deleteParts">キャンセル</button>';
-    // $html[] = '';
+    // 予約キャンセルのモーダル->calenderViewだと見づらい書きづらいからcalendar.bladeに書く
+    // $html[] = '<div class="modal" id="cancelModal">';
+    // $html[] = '<div class="modal__bg"></div>';
+    // $html[] = '<div class="modal__content">';
+    // $html[] = '<p>ファサードとかでモーダルに引っ張った日時を表示</p>';
+    // $html[] = '<p class=".modal-part"></p>';
+    // $html[] = '<p>上記の予約をキャンセルしてもよろしいですか？</p>';
+    // $html[] = '<input type="hidden" name="id" value="" form="deleteParts">';
+    // $html[] = '<form action="/delete/calendar" form="deleteParts" method="post"></form>';
+    // $html[] = '<button class="modal-close btn btn-primary p-0 w-75">閉じる</button>';
+    // $html[] = '<button class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" form="deleteParts">キャンセル</button>';
+    // // $html[] = '';
     $html[] = '<form action="/delete/calendar" method="post" id="deleteParts">'.csrf_field().'</form>';
     $html[] = '</div>';
 
